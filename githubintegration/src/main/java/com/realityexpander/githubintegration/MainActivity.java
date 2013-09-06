76b95696f05478961d3f7da8c5d8ecf3db885bab
realityexpander <realityexpander@gmail.com>
2013-09-06 15:58:48 -0500

    oops didnt mean to add that

--- githubintegration/src/main/java/com/realityexpander/githubintegration/MainActivity.java
+++ githubintegration/src/main/java/com/realityexpander/githubintegration/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i("Hey", "There");
+
         setContentView(R.layout.activity_main);
     }
 
